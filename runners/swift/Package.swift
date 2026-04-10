1fa8399f5ee3def5aa183356f85953f2bf5109b8
Leslie Gutschow <leslie.gutschow@gmail.com>
2026-04-10 16:58:13 -0400

    fix: bump Swift runner to cyphera-swift v0.0.1-alpha.2 (bitWidth + hash fixes)

--- runners/swift/Package.swift
+++ runners/swift/Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "conformance-runner",
     platforms: [.macOS(.v10_15)],
     dependencies: [
-        .package(url: "https://github.com/cyphera-labs/cyphera-swift.git", from: "0.0.1-alpha.1"),
+        .package(url: "https://github.com/cyphera-labs/cyphera-swift.git", from: "0.0.1-alpha.2"),
     ],
     targets: [
         .executableTarget(
